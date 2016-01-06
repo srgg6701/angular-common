98b1ca8e9c0db65ac9001febd7b64b3dd7e33eeb
srgg6701 <srgg67@gmail.com>
2016-01-06 11:54:55 +0300

    Добавление пояснения по способам внедрения зависимостей и подводных камнях.

--- app/controllers/apiTokenController.js
+++ app/controllers/apiTokenController.js
@@ -1,5 +1,9 @@
 "use strict";
 // apiTokenController
+/**
+ * Ахтунг! Не будет работать после минификации/обфускации:
+ * app.controller('apiTokenController', function apiTokenController($scope, apiTokenFactory) {});
+ */
 app.controller('apiTokenController', ['$scope', 'apiTokenFactory', function apiTokenController($scope, apiTokenFactory) {
     console.log([apiTokenFactory]);
     $scope.token = apiTokenFactory.getToken();
