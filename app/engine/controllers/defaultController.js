698cdce43916fee38e3df32d4e979193bb8dd6aa
srgg6701 <srgg67@gmail.com>
2016-01-17 15:45:59 +0300

    Установлен фильтр по полю *name*.

--- app/engine/controllers/defaultController.js
+++ app/engine/controllers/defaultController.js
@@ -2,8 +2,12 @@
 // defaultController
 app.controller('defaultController', function($scope){
     $scope.content="Hello, Dudes!";
+    $scope.greets = [
+        {name:'Hello'}, {name:'hola'}, {name:'hi'}, {name:'Salut!'}, {name:'Здорова!'}, {name:'chiao'}, {name:'bye-bye'}
+    ];
+    $scope.gname = 'name';
     $scope.submitForm = function(event){
-        if(prompt('Input message pls')){
+        if(prompt('Input message plz')){
             var form = event.currentTarget;
             console.log('form', form);
             form.action = '/digest';
