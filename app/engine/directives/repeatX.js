8b330140bafce3655545d0d293a978e1420ed6c7
srgg6701 <srgg67@gmail.com>
2016-01-23 19:07:10 +0300

    Добавлено связывание compile и link.

--- app/engine/directives/repeatX.js
+++ app/engine/directives/repeatX.js
@@ -1,39 +1,74 @@
 "use strict";
-
+/**
+ * Испльзование $compile/
+ * Причина: выражения никогда не компилируются
+ */
 app.directive('repeatX', function(){
     return {
         link: function (scope, el, attributes, controller){
-            console.info('scope, el, attributes, controller', {
+            /*console.info('scope, el, attributes, controller', {
                 scope:scope, el:el, attributes:attributes, controller:controller
-            });
+            });*/
             for( var el_cloned,
                      i= 0,
                  j=Number(attributes.repeatX)-1; i<j;
-                 //i < Number(attributes.repeatX)-1;
-                 i++){ // todo: разобраться. См. 13. Using Compile to Transform the DOM
-
+                 i++){
+                /**
+                 * todo: разобраться. См. 13. Using Compile to Transform the DOM
+                 * бесполезно здесь, необходимо в следующей директиве */
                 el_cloned = el.clone();
 
-                console.log('i, j, el_cloned, repeat-x', {i:i, j:j, el_cloned:el_cloned, attr:el_cloned.attr('repeat-x')});
+                //console.log('i, j, el_cloned, repeat-x', {i:i, j:j, el_cloned:el_cloned, attr:el_cloned.attr('repeat-x')});
 
                 el.after(el_cloned
-                    .attr('repeat-x', 0) /*, false*/ // устанавливает значение параметра в 0
+                    .attr('repeat-x', 0) // устанавливает значение параметра в 0
                 );
             }
         }
     }
 });
-app.directive('repeatY', function(){
+app.directive('repeatY', function($compile){
     return {
         link: function (scope, el, attributes, controller){
             for( var el_cloned,
                      i= 0,
                      j=Number(attributes.repeatY)-1; i<j;
                 i++){
-
                 el_cloned = el.clone();
-
-                el.after(el_cloned.attr('repeat-y', 0));
+                el.after($compile(el_cloned.attr('repeat-y', 0))(scope));
+            }
+        }
+    }
+});
+app.directive('repeatZ', function($compile){
+    return {
+        compile: function (el, attributes){
+            for( var el_cloned,
+                     i= 0,
+                     j=Number(attributes.repeatZ)-1; i<j;
+                 i++){
+                el_cloned = el.clone();
+                el.after(el_cloned.attr('repeat-z', 0));
+            }
+        }
+    }
+});
+app.directive('repeatL', function(){
+    return {
+        compile: function (el, attributes){
+            for( var el_cloned,
+                     i= 0,
+                     j=Number(attributes.repeatL)-1; i<j;
+                 i++){
+                el_cloned = el.clone();
+                el.after(el_cloned.attr('repeat-l', 0));
+            }
+            return function (scope, el, attributes, controller){
+                attributes.$observe('text', function(newValue){
+                    if(newValue=='hello'){
+                        el.css('color', 'red');
+                    }
+                });
             }
         }
     }
